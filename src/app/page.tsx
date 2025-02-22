b4b35387cd52d33147e778929e1427c1ef12dcde
ANT <abouthiam842@gmail.com>
2025-02-22 19:54:01 +0000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>BIENVENUE</h1>
+      
+      <footer className="">
+        
+      </footer>
+    </div>
+  );
+}
